b4c8dbe1f11c01bbf18b3d0d8e60805a3bedcf88
a.bubelev <bubeleu@gmail.com>
2022-06-12 20:54:04 +0300

    pipe

---
+++ src/app-2/app-2.component.ts
@@ -0,0 +1,17 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'my-app';
+//   headerText = 'Create directive';
+//   items = [1, 2, 3, 4, 5];
+//   current = 1;
+//   click(num: number) {
+//     console.log(num);
+//     this.current = num;
+//   }
+// }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,46 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<div>
+                  <!-- <h3>Hi!</h3>x -->
+                  <!-- <div>{{name}}</div>
+                  <div>{{name | lowercase}}</div>
+                  <div>{{name | uppercase}}</div>
+                  <div>{{name | slice:0:3}}</div>
+                  <div>{{name | slice:3:6 | uppercase}}</div> -->
+                  <hr>
+                  <h2>{{pi}}</h2>
+                  <div>{{pi | number}}</div>
+                  <div>{{pi | number:'1.3-4'}}</div>
+                  <div>{{pi | number:'2.2-2'}}</div>
+                  <hr>
+                  <h2>{{money | currency:'EUR'}}</h2>
+                  <hr>
+                  <h2>{{date}}</h2>
+                  <div>{{date | date}}</div>
+                  <div>{{date | date: 'fullDate'}}</div>
+                  <div>{{date | date: 'shortTime'}}</div>
+                  <h2>{{amount}}</h2>
+                  <div>{{amount | percent}}</div>
+                  <hr>
+                  <div>{{object}}</div>
+                  <div>{{object | json}}</div>
+
+              </div>`
 })
 export class AppComponent {
-  title = 'my-app';
-  headerText = 'Create directive';
-  items = [1, 2, 3, 4, 5];
-  current = 1;
-  click(num: number) {
-    console.log(num);
-    this.current = num;
-  }
+  name = 'WebForMyself';
+  pi = Math.PI;
+  money = 350;
+  date = new Date();
+  amount = 0.45;
+  object = {
+    foo: 'bar',
+    baz: 'qux',
+    nested: {
+      xyz: 3,
+      numbers: [1, 2, 3]
+    }
+  };
 }
