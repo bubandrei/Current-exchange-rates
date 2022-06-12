58a0f4a8ce34d04fb20a37f33406facf75d855f8
a.bubelev <bubeleu@gmail.com>
2022-06-12 21:13:19 +0300

    create pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,16 @@
-// import { Component } from "@angular/core";
+import { Component } from "@angular/core";
 
-// @Component({
-//   selector: 'app-root',
-//   template: `<div>
-//                   <h3>Hi!</h3>x
-//                   <div>{{name}}</div>
-//                   <div>{{name | lowercase}}</div>
-//                   <div>{{name | uppercase}}</div>
-//                   <div>{{name | slice:0:3}}</div>
-//                   <div>{{name | slice:3:6 | uppercase}}</div>
-//                   <hr>
-//                   <h2>{{pi}}</h2>
-//                   <div>{{pi | number}}</div>
-//                   <div>{{pi | number:'1.3-4'}}</div>
-//                   <div>{{pi | number:'2.2-2'}}</div>
-//                   <hr>
-//                   <h2>{{money | currency:'EUR'}}</h2>
-//                   <hr>
-//                   <h2>{{date}}</h2>
-//                   <div>{{date | date}}</div>
-//                   <div>{{date | date: 'fullDate'}}</div>
-//                   <div>{{date | date: 'shortTime'}}</div>
-//                   <h2>{{amount}}</h2>
-//                   <div>{{amount | percent}}</div>
-//                   <hr>
-//                   <div>{{object}}</div>
-//                   <div>{{object | json}}</div>
+@Component({
+  selector: 'app-root',
+  template: `<div>
+                  <h3>Hi!</h3>
+                  <h2>{{num}}</h2>
+                  <!-- <h2>{{num | appPow:3}}</h2>
+                  <h2>{{num | appPow:5}}</h2> -->
+                  <h2>{{15 | appPow:3:'='}}</h2>
 
-//               </div>`
-// })
-// export class AppComponent {
-//   name = 'WebForMyself';
-//   pi = Math.PI;
-//   money = 350;
-//   date = new Date();
-//   amount = 0.45;
-//   object = {
-//     foo: 'bar',
-//     baz: 'qux',
-//     nested: {
-//       xyz: 3,
-//       numbers: [1, 2, 3]
-//     }
-//   };
-// }
+              </div>`
+})
+export class AppComponent {
+  num = 2;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PowPipe } from './pow.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PowPipe
   ],
   imports: [
     BrowserModule
