986dd8ca0ee1192071301737712d215992d8eb85
a.bubelev <bubeleu@gmail.com>
2022-06-12 20:01:11 +0300

    [ngSwitch]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  headerText = 'Create directive';
+  items = [1, 2, 3, 4, 5];
+  current = 1;
+  click(num: number) {
+    console.log(num);
+    this.current = num;
+  }
 }
