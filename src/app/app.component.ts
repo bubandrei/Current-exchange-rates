c7d8355b4096ba58a15d3deb94e50bb4ce03ffa2
a.bubelev <bubeleu@gmail.com>
2022-06-18 20:11:30 +0300

    loading ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from "@angular/core";
 import { HttpService } from "./http.service";
 import { Currency } from "./currency";
 import { catchError, of } from "rxjs";
+import { MessageService } from "primeng/api";
 
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
-  providers: [HttpService]
+  providers: [HttpService, MessageService]
 })
 export class AppComponent implements OnInit {
 
@@ -16,6 +17,7 @@ export class AppComponent implements OnInit {
   current: Currency[] = [];
   call: any = [];
   dateSelected: string = '';
+  loading: boolean = true;
 
   constructor(private httpService: HttpService) { }
 
@@ -25,7 +27,7 @@ export class AppComponent implements OnInit {
         .getData(this.dateSelected)
         // .subscribe((data: any) => console.log(data[0]["table"]));
         .subscribe((data: any) =>
-          (this.current = data[0]["rates"], this.call = data[0]));
+          (this.current = data[0]["rates"], this.call = data[0], this.loading = false));
     } catch (error) {
       console.log('tttt')
     }
@@ -34,10 +36,11 @@ export class AppComponent implements OnInit {
 
   }
   fetchDate() {
+    this.loading =true;
     this.httpService
       .getData(this.dateSelected)
       // .subscribe((data: any) => console.log(data[0]["table"]));
-      .subscribe((data: any) => (this.current = data[0]["rates"], this.call = data[0]));
+      .subscribe((data: any) => (this.current = data[0]["rates"], this.call = data[0], this.loading = false));
   }
 }
 function data(data: any) {
