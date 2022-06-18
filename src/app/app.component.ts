1969b443cff86b50d3b9d487ea8260f3ebc71d85
a.bubelev <bubeleu@gmail.com>
2022-06-19 01:11:12 +0300

    ready without clean

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,37 +18,28 @@ export class AppComponent implements OnInit {
   call: any = [];
   dateSelected: string = '';
   loading: boolean = true;
-  data: any;
+  error: string = '';
 
   constructor(private httpService: HttpService) { }
 
   ngOnInit() {
     try {
       const http$ = this.httpService.getData(this.dateSelected);
-      // .subscribe((data: any) => console.log(data[0]["table"]));
-      // http$.subscribe((data: any) => {
-      //   (this.current = data[0]["rates"], this.call = data[0], this.loading = false);
-      // }, (error) => {
-      //   console.log(error, 'Not works');
-      // }
-      // );
       http$.pipe(
         catchError((err) => {
-          // console.log('rethrowing error', err);
           return throwError(() => new Error(err.message));
         }),
         catchError((err) => {
-          // console.log('caught rethrown error', err);
-          return of('Http call failed');
+          return of(this.error = 'Http call failed');
         }),
       )
         .subscribe({
           next: (data: any) => {
-            (this.data = data, this.current = data[0]["rates"], this.call = data[0], this.loading = false)
+            (this.current = data[0]["rates"], this.call = data[0], this.loading = false)
           }
         });
     } catch (err) {
-      console.log(err, 'second');
+      console.log(err);
     } finally {
       console.log('sync finally block');
     }
@@ -56,10 +47,28 @@ export class AppComponent implements OnInit {
 
   fetchDate() {
     this.loading = true;
-    const http$ = this.httpService.getData(this.dateSelected);
-    // .subscribe((data: any) => console.log(data[0]["table"]));
-    http$.subscribe((data: any) => (this.current = data[0]["rates"], this.call = data[0], this.loading = false));
+    this.error = '';
+    try {
+      const http$ = this.httpService.getData(this.dateSelected);
+      http$.pipe(
+        catchError((err) => {
+          return throwError(() => new Error(err.message));
+        }),
+        catchError((err) => {
+          return of(this.error = 'Http call failed');
+        }),
+      )
+        .subscribe({
+          next: (data: any) => {
+            (this.current = data[0]["rates"], this.call = data[0], this.loading = false)
+          }
+        });
+    } catch (err) {
+      console.log(err);
+    } finally {
+      console.log('sync finally block');
+    }
+
   }
 
 }
-
